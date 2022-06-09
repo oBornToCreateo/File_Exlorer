823cc1f90e7abfbdd0bce7440be26ceed1d50dc6
oBornToCreateo <Kjezier@gmail.com>
2022-06-09 18:40:52 +0200

    Changes, moved iteration to the function, input with polish signs causes erorrs

--- File_Explorer.cpp
+++ File_Explorer.cpp
@@ -4,8 +4,43 @@
 #include <iostream>
 #include <filesystem>
 #include <locale>
+ 
 
 namespace fs = std::filesystem;
+ 
+
+
+std::vector<std::pair<long long, std::wstring>> IterateThroughtAndDisplayContents(std::wstring path){
+
+    int item_number = 1;
+    std::vector<std::pair<long long, std::wstring>>  Names;
+
+    for (const auto& entry : fs::directory_iterator(path)) {
+        auto pathfiles_names   = entry.path().filename().wstring();
+        auto pathfiles_size    = entry.file_size();
+        auto pathfiles_regtype = entry.is_regular_file();
+        auto pathfiles_dirtype = entry.is_directory();
+
+        //--------------------[DSPLAY DIR CONTENT]--------------------------
+        
+        std::wcout.width(6); std::wcout << item_number << L". ";
+        if (pathfiles_regtype)
+            std::wcout << L"[FILE]" << L" ";
+        else if(pathfiles_dirtype)
+            std::wcout << L"[DIR]" << L" ";
+        std::wcout<< pathfiles_names << std::endl;
+        std::wcout << std::endl;
+
+        //------------------[PREPARE FOR COMPARASION]----------------------
+
+        Names.push_back(std::make_pair(pathfiles_size, pathfiles_names));
+        item_number++;
+    }
+    item_number = 1;
+
+    return Names;
+}
+
 
 int main() {
 
@@ -16,21 +51,13 @@ int main() {
 
         do {
             std::wstring path;
-            int item_number = 1;
-            std::vector<std::pair<int,std::wstring>>  Names_Old, Names_New;
+            std::vector<std::pair<long long, std::wstring>>  Names_Old, Names_New;
+            
             std::wcout << L"enter directory path : ";
             std::wcin >> path; std::cout << std::endl;
-
             std::wcout << L"Choosen directory content" << std::endl << std::endl;
 
-            for (const auto& entry : fs::directory_iterator(path)) {
-                auto pathfiles_names = entry.path().filename().wstring();
-                auto pathfiles_size = entry.file_size();
-                std::wcout << item_number << L" " << pathfiles_names << std::endl;
-                Names_Old.push_back(std::make_pair(pathfiles_size,pathfiles_names));
-                item_number++;
-            }
-            item_number = 1;
+            Names_Old = IterateThroughtAndDisplayContents(path);
 
             do {
                 std::wcout << std::endl;
@@ -40,16 +67,11 @@ int main() {
                 std::wcin >> choise;
 
                 if (choise == L"Y") {
-                    for (const auto& entry : fs::directory_iterator(path)) {
-                        auto pathfiles_names = entry.path().filename().wstring();
-                        auto pathfiles_size = entry.file_size();
-                        std::wcout << item_number << L" " << pathfiles_names << std::endl;
-                        Names_New.push_back(std::make_pair(pathfiles_size, pathfiles_names));
-                        item_number++;
-                    }
-                    item_number = 1;
+
+                    Names_New = IterateThroughtAndDisplayContents(path);
 
                     if (Names_New == Names_Old)
+                    
                         std::cout << " There are no changes in the contents " << std::endl;
                     else
                         std::cout << " There are changes in the contents " << std::endl;
@@ -58,9 +80,10 @@ int main() {
 
                 }
                 else if (choise == L"N")
-                    break; // display beginning
+                    break;  
 
                 else
+                    std::wcout << std::endl;
                     std::wcout << L"Wrong command !" << std::endl;
 
             } while (true);
